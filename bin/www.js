c26ac10462b216e077c25e7b4a49c4e5dafc4d92
chubo <chubo@4paradigm.com>
2018-10-21 22:33:11 +0800

    update

--- bin/www.js
+++ bin/www.js
@@ -12,7 +12,7 @@ var https = require('https');
 var http = require('http');
 
 var httpsPort = 5887; // 自定义数字端口，给https用的端口
-var httpPort = 5888; // 自定义数字端口
+var httpPort = 80; // 自定义数字端口
 // 1、创建https服务器
 var privateKey = fs.readFileSync(path.join(__dirname, './1538108319871.key'), 'utf8');
 var certificate = fs.readFileSync(path.join(__dirname, './1538108319871.pem'), 'utf8');
